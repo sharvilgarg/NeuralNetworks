e697a539e1021ad41afa260981c11c0f4adecbd4
sharvilgarg <sharvil.garg@gmail.com>
2019-07-11 11:11:59 +0530

    Initial Commit

---
+++ Perceptron/perceptron.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+feature_set = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0], [1, 1, 0], [1, 1, 1]])
+labels = np.array([[1, 0, 0, 1, 1]])
+labels = labels.reshape(5, 1)
+
+np.random.seed(42)
+weights = np.random.rand(3, 1)
+bias = np.random.rand(1)
+lr = 0.05
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_der(x):
+    return sigmoid(x)*(1-sigmoid(x))
+
+
+for epoch in range(2000):
+    inputs = feature_set
+
+    XW = np.dot(feature_set, weights) + bias
+    z = sigmoid(XW)
+
+    error = z - labels
+    print(error.sum())
+
+    dcost_dpred = error
+    dpred_dz = sigmoid_der(z)
+    z_delta = dcost_dpred * dpred_dz
+    inputs = feature_set.T
+    weights -= lr * np.dot(inputs, z_delta)
+    for num in z_delta:
+        bias -= lr * num
